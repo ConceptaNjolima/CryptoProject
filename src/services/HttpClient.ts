fe1acbdc421725abe5c9536ca24c210669c46613
Concepta Tugume <cnjolima@microsoft.com>
2025-11-13 08:25:32 +0000

    move API key from being hard-coded

--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const httpClient = axios.create({
     baseURL: "https://api.coingecko.com/api/v3/coins",
     headers: {
-        "x-cg-demo-api-key": "CG-LMtRXdChMyiunMdnAp8hAj9f"
+        "x-cg-demo-api-key": import.meta.env.VITE_COIN_GECKO_API_KEY || ""
     }
-})
+});
